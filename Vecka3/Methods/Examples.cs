fbb80436c1545c32ae2f75afe987908383d241c2
BubbaJJ <55495429+BubbaJJ@users.noreply.github.com>
2021-10-18 10:51:25 +0200

    Vecka3

---
+++ Vecka3/Methods/Examples.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Vecka3.Methods
+{
+    static class Methods
+    {
+        public static void Examples()
+        {
+            /*
+                Syntax:
+                returtyp MetodNamn(ev. indata)
+                {
+                    Kod här
+                }
+
+                void MetodNamn()
+                {
+                    Gör något.
+                }
+
+                int MetodNamn()
+                {
+                    return ...
+                }
+
+                Namnet för indata existerar endast inom metoden.
+                Namn utanför metoden existerar inte i metoden.
+
+                MetodNamn(ev indata) för att anropa metoden.
+            */
+
+            void PrintSomeInfo()
+            {
+                Console.WriteLine("Information");
+            }
+
+            PrintSomeInfo();
+
+            void PrintMyInfo(string name)
+            {
+                Console.WriteLine("I'm {0}.", name);
+            }
+
+            PrintMyInfo("Jim");
+
+            int GetNumber()
+            {
+                int number = 10;
+                return number;
+            }
+
+            Console.WriteLine(GetNumber());
+
+            string ChangeName(string name)
+            {
+                name += " J!";
+                return name;
+            }
+
+            string name = ChangeName("Bubba");
+            Console.WriteLine(name);
+        }
+    }
+}
