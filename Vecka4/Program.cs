b96d313b78469c1c3d4d8b4e5e81a5abc2b03848
Jim Kaya <jim.kaya@gmail.com>
2021-10-28 13:13:36 +0200

    Demo code for repetition.

--- Vecka4/Program.cs
+++ Vecka4/Program.cs
@@ -7,6 +7,262 @@ namespace Vecka4
     {
         static void Main(string[] args)
         {
+            string[] weekdays = {"Måndag", "Tisdag", "Onsdag" };
+            Console.WriteLine("Välj dag: [1-3] ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(weekdays[choice-1]); 
+            /*
+            List<int> numbers = new List<int>() {1,2,3,4,5,6,7,8,9,10 };
+
+            Console.WriteLine(numbers.Count);
+
+            numbers.Reverse();
+
+            foreach(int number in numbers)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+            numbers.Insert(0,23);
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+            Console.WriteLine(numbers.Count);
+
+            numbers.Sort();
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+
+            numbers.RemoveAll(item => item%2 == 0);
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+
+            numbers.AddRange(new int[] {23,2,24 });
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+            Console.WriteLine();
+            */
+            /*
+            int[,] numbers = new int[3,2] {{1,2 },{3,4 },{5,6 } };
+            int[] numbersRad0 = new int[] { 1, 2 };
+            int[] numbersRad1 = new int[] { 3, 4 };
+            int[] numbersRad2 = new int[] { 5, 6 };
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.WriteLine(numbers[i,1]);
+            }
+
+            Console.WriteLine(numbers[0, 0]); // Rad 0, Kolumn 0 == 1
+            Console.WriteLine(numbers[0, 1]); // Rad 0, Kolumn 1 == 2
+            Console.WriteLine(numbers[1, 0]); // Rad 1, Kolumn 0 == 3
+            Console.WriteLine(numbers[1, 1]); // Rad 1, Kolumn 1 == 4
+            Console.WriteLine(numbers[2, 0]); // Rad 2, Kolumn 0 == 5
+            Console.WriteLine(numbers[2, 1]); // Rad 2, Kolumn 1 == 6
+            */
+            /*
+            int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10 };
+            int[] numbersEmpty = new int[5];
+
+            for (int i = 0; i < numbersEmpty.Length; i++)
+            {
+                Console.WriteLine("Skriv in tal för index {0}: ",i);
+                numbersEmpty[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int number in numbersEmpty)
+            {
+                Console.WriteLine(number);
+            }
+            */
+            // Skriv ut alla tal 0-100 med en for-loop
+            /*
+            for (int i = 0;i<=100;i++)
+            {
+                Console.WriteLine("i = {0}",i);
+            }
+            */
+            // Skriv ut alla tal 100-0 med en for-loop
+            /*
+            for (int i = 100; i >= 0; i--)
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+            */
+            // Skriv ut alla jämna tal 0-100 med en for-loop
+            /*
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("i = {0}", i);
+                }
+            }
+            */
+            /*
+            for (int i = 1; i <= 99; i += 2)
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+            */
+            // Skriv ut alla ojämna tal mellan 1-99 med for-loop
+
+
+
+            /*
+            int[] numbers = {1,2,3,4,5 };
+            
+            for (int i = 0;i<=10 ;i++) // Börja räkna från 0, så länge i är mindre än 10 - kör loopen, öka i med 1 för varje loop
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+            int age = 30;
+            int stop = 65;
+
+            while (age<stop)
+            {
+                Console.WriteLine(age);
+            }
+            /*
+            foreach (int num in numbers)
+            {
+                if (num == 3)
+                {
+                Console.WriteLine(num);
+                }
+            }
+            */
+
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                continue;
+                for (int j = 0; j < 5; j++)
+                {
+                if (j % 2 != 0)
+                {
+                    break;
+                }
+                Console.WriteLine(j);
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+            */
+            /*
+            // Skapa en variabel som heter memory, värde 0
+            int memory = 0;
+
+            // Skapa en variabel som heter value, värde 0
+            int value = 0;
+
+            // While-loop, villkor value >= 0
+            while(value >= 0)
+            {
+                // Be användare om värde
+                Console.Write("Skriv in valfritt tal: ");
+                // Omvandla, och lagra, värdet
+                value = Convert.ToInt32(Console.ReadLine());
+                // Addera till memory
+                memory += value; // memory = memory + value;
+                // memory = memory: 10 + value: 5 = 15
+                // memory = memory: 15 + value: 7 = 22
+                Console.WriteLine(memory);
+            }
+
+            // När loopen bryts, skriv ut summan
+            Console.WriteLine("Total summa: {0}", memory);
+
+            */
+
+            /*
+            // Be användaren om ålder
+            Console.Write("Skriv in din ålder: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Antal år till pension
+            int retirementAge = 65;
+            int yearsLeft = retirementAge - age;
+            Console.WriteLine("Du har {0} år kvar till pension.", yearsLeft);
+            
+            // Fråga om att spola fram tiden
+            // Räkna upp till pension
+            Console.Write("Vill du spola fram till pension? [J/N] ");
+            char choice = Convert.ToChar(Console.ReadLine());
+            if(choice == 'J')
+            {
+                int originalAge = age;
+                // Skriv ut ålder varje år
+                while (age<retirementAge)
+                {
+                    age++;
+                    Console.WriteLine("Du är nu {0} år.",age);
+                }
+
+                Console.Write("Vill du stanna kvar här? J eller N ");
+                choice = Convert.ToChar(Console.ReadLine());
+
+                if(choice == 'N')
+                {
+                    while (age>originalAge)
+                    {
+                        age--;
+                        Console.WriteLine("Du är nu {0} år.", age);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Okej, njut av pensionen!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Okej, njut av livet!");
+            }
+            Console.WriteLine("Tack för att du deltog!");
+
+            
+
+            // Fråga om att spola tillbaka
+
+            // Om ja, räkna ner, skriv ut ålder
+
+            // Om nej, avsluta
+
+            // Skriv ut något..
+
+            */
+
+            /*
+
+            void SwapVariables() { 
+            int a = 4;
+            int b = 7;
+            int c; // Temporär lagring
+
+            c = a;   // temp = 4
+            a = b;   // a = 7
+            b = c;   // b = 4
+            }
+
+            SwapVariables();
+            */
             // 1:
             /*
             static void Greeting()
@@ -26,7 +282,7 @@ namespace Vecka4
 
             GreetingInput(name);
             */
-            int userAge = 18;
+            //int userAge = 18;
 
             /*
             static bool VerifyAge(int age)
@@ -68,7 +324,7 @@ namespace Vecka4
 
             Console.WriteLine(AddSum(2,5));
             */
-
+            /*
             static void AreaSquare(int b)
             {
                 int area = b * b;
@@ -111,8 +367,8 @@ namespace Vecka4
             }
             Console.WriteLine("Number of throws: ");
             int userThrows = Convert.ToInt32(Console.ReadLine());
-
-            Dice(userThrows);
+            */
+            //Dice(userThrows);
 
             /*
             static int CountNumbers(string str)
@@ -395,10 +651,10 @@ namespace Vecka4
             */
 
 
-            /*
-             * Foreach övning 11:
 
-            string input = "kajak";
+            //Foreach övning 11:
+            /*
+            string input = "kajrak";
             List<char> inputList = new List<char>();
             inputList.AddRange(input);
 
@@ -411,10 +667,18 @@ namespace Vecka4
             inputReversed.AddRange(input);
             inputReversed.Reverse();
 
+            if (inputList == inputReversed)
+            {
+                Console.WriteLine("test");
+            }
+
             for (int i = 0; i < inputList.Count/2; i++)
             // Loopen igenom första halvan av listan.
             {
-                if(inputList[i] == inputList[inputList.Count - (i - 1)])
+                if(inputList[i] == inputList[inputList.Count - (i + 1)])
+                // count = 5        i = 0 + 1 == 1 /Count - 1
+                // count = 5        i = 1 + 1 == 2 /Count - 2
+
                 // inputList.Count = 5
                 // inputList.Count - 0 = 5
                 // inputList.Count - 0 - 1 = 4
@@ -428,8 +692,8 @@ namespace Vecka4
                     continue;
                 }
             }
+            
             */
-
             /*
              * Foreach övning 18:
             string name = "Handelsakademin";
