fa1bb541f868bc7e6e688ec8677b62fed5bca355
Jim Kaya <jim.kaya@gmail.com>
2021-11-08 10:53:17 +0100

    Demo examples added.

---
+++ Vecka5/Demo/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vecka5.Demo
+{
+    class Converter
+    {
+        public Converter()
+        {
+        }
+
+        // Hör till objektet
+        public void SaySomething()
+        {
+            Console.WriteLine("Something!");
+        }
+
+        // Hör till klassen
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
---
+++ Vecka5/Demo/HumanPublic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vecka5.Demo
+{
+    class HumanPublic
+    {
+        // Fields
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+        private string _location;
+        private bool _isParent;
+
+        // Auto-implemented Property (get & set)
+        public double Height { get; }
+
+        // Metod
+        public void PrintInfo()
+        {
+            Console.WriteLine("First name: {0}", _firstName);
+            Console.WriteLine("Last name: {0}", _lastName);
+            Console.WriteLine("Age: {0}", _age);
+            Console.WriteLine("Location: {0}", _location);
+            Console.WriteLine("Parent: {0}", _isParent);
+            Console.WriteLine("Height: {0}", Height);
+        }
+
+        // Property (get & set)
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        // Konstruktor
+        public HumanPublic()
+        {
+        }
+
+        public HumanPublic(string firstName, string lastName, int age, string location, bool isParent, double height)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._age = age;
+            this._location = location;
+            this._isParent = isParent;
+            this.Height = height;
+        }
+    }
+}
