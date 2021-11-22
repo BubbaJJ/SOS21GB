2295f02c4273be8f81a8db4031be784e11280ee3
Jim Kaya <jim.kaya@gmail.com>
2021-11-22 09:53:35 +0100

    Added execution of solutions.

--- Vecka2/Program.cs
+++ Vecka2/Program.cs
@@ -2,10 +2,110 @@
 
 namespace Vecka2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // Arrays & Listor övningar:
+            Console.WriteLine("Arrays & Listor");
+            // Övning 1:
+            Arrays.Exercise01.Solution();
+            // Övning 2
+            Arrays.Exercise02.Solution();
+            // Övning 3
+            Arrays.Exercise03.Solution();
+            // Övning 4
+            Arrays.Exercise04.Solution();
+            // Övning 5
+            Arrays.Exercise05.Solution();
+            // Övning 6
+            Arrays.Exercise06.Solution();
+            // Övning 7
+            Arrays.Exercise07.Solution();
+            // Övning 8
+            Arrays.Exercise08.Solution();
+            // Övning 9
+            Arrays.Exercise09.Solution();
+            // Övning 10
+            Arrays.Exercise10.Solution();
+            // Övning 11
+            Arrays.Exercise11.Solution();
+            // Övning 12
+            Arrays.Exercise11.Solution();
+
+            // While & do while övningar:
+            Console.WriteLine("While & Do while");
+            // Övning 1
+            WhileDoWhile.Exercise01.Solution();
+            // Övning 2
+            WhileDoWhile.Exercise02.Solution();
+            // Övning 3
+            WhileDoWhile.Exercise03.Solution();
+            // Övning 4
+            WhileDoWhile.Exercise04.Solution();
+            // Övning 5
+            WhileDoWhile.Exercise05.Solution();
+            // Övning 6
+            WhileDoWhile.Exercise06.Solution();
+            // Övning 7
+            WhileDoWhile.Exercise07.Solution();
+            // Övning 8
+            WhileDoWhile.Exercise08.Solution();
+            // Övning 9
+            WhileDoWhile.Exercise09.Solution();
+            // Övning 10
+            WhileDoWhile.Exercise10.Solution();
+            // Övning 11
+            WhileDoWhile.Exercise11.Solution();
+            // Övning 12
+            WhileDoWhile.Exercise12.Solution();
+            // Övning 13
+            WhileDoWhile.Exercise13.Solution();
+
+            // For & foreach övningar:
+            Console.WriteLine("For & Foreach");
+            // Övning 1
+            ForEach.Exercise01.Solution();
+            // Övning 2
+            ForEach.Exercise02.Solution();
+            // Övning 3
+            ForEach.Exercise03.Solution();
+            // Övning 4
+            ForEach.Exercise04.Solution();
+            // Övning 5
+            ForEach.Exercise05.Solution();
+            // Övning 6
+            ForEach.Exercise06.Solution();
+            // Övning 7
+            ForEach.Exercise07.Solution();
+            // Övning 8
+            ForEach.Exercise08.Solution();
+            // Övning 9
+            ForEach.Exercise09.Solution();
+            // Övning 10
+            ForEach.Exercise10.Solution();
+            // Övning 11
+            ForEach.Exercise11.Solution();
+            // Övning 12
+            ForEach.Exercise12.Solution();
+            // Övning 13
+            ForEach.Exercise13.Solution();
+            // Övning 14
+            ForEach.Exercise14.Solution();
+            // Övning 15
+            ForEach.Exercise15.Solution();
+            // Övning 16
+            ForEach.Exercise16.Solution();
+            // Övning 17
+            ForEach.Exercise17.Solution();
+            // Övning 18
+            ForEach.Exercise18.Solution();
+            // Övning 19
+            ForEach.Exercise19.Solution();
+            // Övning 20
+            ForEach.Exercise20.Solution();
+            // Övning 21
+            ForEach.Exercise21.Solution();
         }
     }
 }
